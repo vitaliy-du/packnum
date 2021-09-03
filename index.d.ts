88a6712fa62ac66dc0cb9b7bcea39c3011e9160c
Vitaliy Dyukar <vitaliy.du@yandex.ru>
2021-09-04 01:17:05 +0300

    Add files. version 1.2.7

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Return numeric value of string.
+ *
+ * @param value PackNum encoded string.
+ */
+export function packNumDecode(value: string): number;
+
+/**
+ * Return string value of number.
+ *
+ * @param value Numeric value.
+ */
+export function packNumEncode(value: number): string;
